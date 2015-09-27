07728e80e4381bf1219f90e44e276f2b8e7079ef
Arnold Daniels <arnold@jasny.net>
2015-09-27 12:08:57 +0200

    ipv6 support for ip_in_cidr

--- src/server_functions.php
+++ src/server_functions.php
@@ -3,7 +3,9 @@
 namespace Jasny;
 
 /**
- * Check if an IP is in CIDR block
+ * Check if IP address is in CIDR block
+ * 
+ * @todo Convert IPv4 to IPv6 and visa versa (when possible)
  *
  * @param string $ip
  * @param string $cidr
@@ -11,19 +13,78 @@ namespace Jasny;
  */
 function ip_in_cidr($ip, $cidr)
 {
-    $parts = explode('/', $cidr);
+    if ($cidr === '0.0.0.0/0' || $cidr === '::/0' || $cidr === '::') return true;
+
+    $ipv = strpos($ip, ':') === false ? 4 : 6;
+    $cidrv = strpos($cidr, ':') === false ? 4 : 6;
+     
+    if ($ipv !== $cidrv) return false;
     
-    $ipc_bytes = explode('.', $parts[0]);
-    foreach ($ipc_bytes as &$v) {
-        $v = str_pad(decbin($v), 8, '0', STR_PAD_LEFT);
-    }
-    $ipc = substr(join('', $ipc_bytes), 0, $parts[1]);
-    
-    $ipu_bytes = explode('.', $ip);
-    foreach ($ipu_bytes as &$v) {
-        $v = str_pad(decbin($v), 8, '0', STR_PAD_LEFT);
-    }
-    $ipu = substr(join('', $ipu_bytes), 0, $parts[1]);
-    
-    return $ipu == $ipc;
+    $fn = __NAMESPACE__ . "\\ipv{$ipv}_in_cidr";
+    return $fn($ip, $cidr);
 }
+
+/**
+ * Check if IPv4 address is in CIDR block
+ * 
+ * @param string $ip
+ * @param string $cidr
+ * @return boolean
+ */
+function ipv4_in_cidr($ip, $cidr)
+{
+    list($subnet, $mask) = explode('/', $cidr);
+    
+    $ipMasked = ip2long($ip) & ~((1 << (32 - $mask)) - 1);
+    $subnetMasked = ip2long($subnet) & ~((1 << (32 - $mask)) - 1);
+    
+    return $ipMasked == $subnetMasked;
+}
+
+/**
+ * Converts inet_pton output to string with bits
+ *
+ * @param string $inet
+ * @return string
+ */
+function inet_to_bits($inet)
+{
+   $unpacked = unpack('A16', $inet);
+   $unpacked = str_split($unpacked[1]);
+   $binaryip = '';
+
+   foreach ($unpacked as $char) {
+             $binaryip .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT);
+   }
+   
+   return $binaryip;
+}    
+
+/**
+ * Check if IPv6 address is in CIDR block
+ * 
+ * @param string $ip
+ * @param string $cidr
+ * @return boolean
+ */
+function ipv6_in_cidr($ip, $cidr)
+{
+    $inetIp = inet_pton($ip);
+    $binaryIp = inet_to_bits($inetIp);
+
+    if (strpos($cidr, '/') === false) {
+        $net = $cidr;
+        $mask = $cidr === '::' ? 0 : substr_count(rtrim($cidr, ':'), ':') * 16;
+    } else {
+        list($net, $mask) = explode('/', $cidr);
+    }
+
+    $inetNet = inet_pton($net);
+    $binaryNet = inet_to_bits($inetNet);
+
+    $ipNetBits = substr($binaryIp, 0, $mask);
+    $netBits = substr($binaryNet, 0, $mask);
+
+    return $ipNetBits === $netBits;
+}
+
--- tests/ServerFunctionsTest.php
+++ tests/ServerFunctionsTest.php
@@ -8,19 +8,55 @@ namespace Jasny;
 class ServerFunctionsTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * test ipv4_in_cidr
+     */
+    public function testIpv4InCidr()
+    {
+        $this->assertTrue(ipv4_in_cidr('192.168.0.1', '192.168.0.1/32'));
+        $this->assertTrue(ipv4_in_cidr('192.168.0.1', '192.168.0.0/24'));
+        $this->assertTrue(ipv4_in_cidr('192.168.0.1', '192.168.0.10/24'));
+        $this->assertTrue(ipv4_in_cidr('192.168.0.1', '192.128.0.0/9'));
+        $this->assertTrue(ipv4_in_cidr('192.168.0.1', '0.0.0.0/0'));
+        
+        $this->assertFalse(ipv4_in_cidr('192.168.0.1', '192.168.0.10/32'));
+        $this->assertFalse(ipv4_in_cidr('192.168.0.1', '192.168.1.0/24'));
+        $this->assertFalse(ipv4_in_cidr('192.168.0.1', '192.64.0.0/9'));
+    }
+
+    /**
+     * test ipv6_in_cidr
+     */    
+    public function testIpv6InCidr()
+    {
+        $this->assertTrue(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2F3B::/64'));
+        $this->assertTrue(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2F3B:0000:0000:0000:0000/64'));
+        $this->assertTrue(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2F3B:0200::/72'));
+        $this->assertTrue(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2F3B::'));
+        $this->assertTrue(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '::'));
+    
+        $this->assertFalse(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2000::/64'));
+        $this->assertFalse(ipv6_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2F3B:9900::/72'));
+    }
+    
+    /**
      * test ip_in_cidr
+     *
+     * @depends testIpv4InCidr
+     * @depends testIpv6InCidr
      */
     public function testIpInCidr()
     {
         $this->assertTrue(ip_in_cidr('192.168.0.1', '192.168.0.1/32'));
-        $this->assertTrue(ip_in_cidr('192.168.0.1', '192.168.0.0/24'));
-        $this->assertTrue(ip_in_cidr('192.168.0.1', '192.168.0.10/24'));
-        $this->assertTrue(ip_in_cidr('192.168.0.1', '192.128.0.0/9'));
-        $this->assertTrue(ip_in_cidr('192.168.0.1', '0.0.0.0/0'));
+        $this->assertTrue(ip_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2F3B::/64'));
         
         $this->assertFalse(ip_in_cidr('192.168.0.1', '192.168.0.10/32'));
-        $this->assertFalse(ip_in_cidr('192.168.0.1', '192.168.1.0/24'));
-        $this->assertFalse(ip_in_cidr('192.168.0.1', '192.64.0.0/9'));
+        $this->assertFalse(ip_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '21DA:00D3:0000:2000::/64'));
+        
+        $this->assertFalse(ip_in_cidr('192.168.0.1', '21DA:00D3:0000:2F3B::/64'));
+        $this->assertFalse(ip_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '192.168.0.10/32'));
+        
+        $this->assertTrue(ip_in_cidr('192.168.0.1', '::'));
+        $this->assertTrue(ip_in_cidr('21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A', '0.0.0.0/0'));
     }
 }
 
