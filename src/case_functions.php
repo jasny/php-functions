243a2044e975b984a37bb033c079d541f15e2e2b
Arnold Daniels <arnold@jasny.net>
2017-01-19 06:45:55 +0100

    Improved case functions Uncase not longer ucfirst

--- src/case_functions.php
+++ src/case_functions.php
@@ -60,7 +60,6 @@ function uncase($string)
 {
     $snake = strtolower(preg_replace('/([a-z0-9])([A-Z])/', '$1 $2', $string));
     $sentence = preg_replace('/[-_\s]+/', ' ', $snake);
-    if (ctype_upper($string[0])) $sentence = ucfirst($sentence);
     
     return $sentence;
 }
